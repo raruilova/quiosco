85f7a5d4d9824cf4f51bddaab534d11833004e89
robinson <robinjosellove@gmail.com>
2024-03-28 22:17:15 -0500

    zustand order and add order

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { useStore } from "@/src/store";
+import { ProductDetails } from "./ProductDetails";
+
 export const OrderSummary = () => {
+    const order = useStore((state) => state.order);
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-bold">Mi pedido</h1>
+            {order.length === 0 ? <p className="text-center my-10">El carrito esta vacío</p> : (
+                <div className="mt-5">
+                    {order.map(item => (
+                        <ProductDetails key={item.id}
+                        item={item}
+                        />
+                    ))}
+                </div>
+            )}
         </aside>
     )
 }
---
+++ src/store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { OrderItem } from "./types";
+import { Product } from "@prisma/client";
+
+interface Store {
+    order: OrderItem[]
+    addToOrder: (product: Product) => void
+}
+
+export const useStore = create<Store>((set, get) => ({
+    order: [],
+    addToOrder: (product) => {
+        const {categoryId, image, ...data} = product;
+        let order:OrderItem[] = [];
+        if(get().order.find(item => item.id === data.id)) {
+            order = get().order.map(item => item.id === data.id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            }: item);
+        } else {
+           order = [...get().order, {
+                ...data,
+                quantity: 1,
+                subtotal: 1 * product.price
+            }]
+        }
+        //...data con los datos que quiero añadir, los demas los separo
+        set(() => ({
+            order
+        }))
+    }
+}));
