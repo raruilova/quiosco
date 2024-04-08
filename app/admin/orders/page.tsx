7bf84c45da03ad18d9aa4011372f0504983300b2
robinson <robinjosellove@gmail.com>
2024-04-08 18:33:36 -0500

    refresh page manual

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import { OrderCard } from "@/components/order/OrderCard";
 import { Heading } from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 const getPendingOrders = async () => {
     const orders = await prisma.order.findMany({
@@ -21,9 +22,17 @@ const getPendingOrders = async () => {
 
 const AdminPage = async () => {
     const orders = await getPendingOrders();
+    const refreshOrders = async () => {
+        "use server"
+        revalidatePath("/admin/orders");
+    }
     return (
         <>
         <Heading>Administrar Ordenes</Heading>
+        <form action={refreshOrders}>
+            <input type="submit" value="Actualizar ordenes"           className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+/>
+        </form>
         {orders.length ? (
             <div className="grip grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
                 {orders.map(order => (
